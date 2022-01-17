497c13d1e87a6521ff38fe56b0be4c0df16701c2
Ca1doz <vladislavlazutkin64@gmail.com>
2022-01-17 09:47:31 +0300

    уравнение

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,9 +6,11 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadLine();
-            Console.ReadLine();
+            int x, y;
+            Console.WriteLine("Введите значение x");
+            x = Convert.ToInt32(Console.ReadLine());
+            y = 5 * x;
+            Console.WriteLine(y);
         }
     }
 }
